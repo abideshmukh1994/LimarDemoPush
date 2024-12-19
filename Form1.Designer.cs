03fafbd6e0d51eb4d6971dbd9fa525d31eb51451
VSO <VSO@DESKTOP-1FBQQO7>
2024-12-19 15:36:54 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace LimarDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.dataGridViewImages = new System.Windows.Forms.DataGridView();
+            this.progressBarUpload = new System.Windows.Forms.ProgressBar();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewImages)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Folder";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(210, 45);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(222, 20);
+            this.txtFolderPath.TabIndex = 1;
+            // 
+            // dataGridViewImages
+            // 
+            this.dataGridViewImages.AllowUserToAddRows = false;
+            this.dataGridViewImages.AllowUserToDeleteRows = false;
+            this.dataGridViewImages.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewImages.Location = new System.Drawing.Point(138, 90);
+            this.dataGridViewImages.Name = "dataGridViewImages";
+            this.dataGridViewImages.ReadOnly = true;
+            this.dataGridViewImages.Size = new System.Drawing.Size(375, 150);
+            this.dataGridViewImages.TabIndex = 3;
+            // 
+            // progressBarUpload
+            // 
+            this.progressBarUpload.Location = new System.Drawing.Point(138, 262);
+            this.progressBarUpload.Name = "progressBarUpload";
+            this.progressBarUpload.Size = new System.Drawing.Size(294, 23);
+            this.progressBarUpload.TabIndex = 4;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(438, 262);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 5;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(438, 45);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFolder.TabIndex = 6;
+            this.btnSelectFolder.Text = "Select";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.progressBarUpload);
+            this.Controls.Add(this.dataGridViewImages);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewImages)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.DataGridView dataGridViewImages;
+        private System.Windows.Forms.ProgressBar progressBarUpload;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnSelectFolder;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LimarDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFolderPath.Text = folderDialog.SelectedPath;
+
+                    // Get image files and bind to DataGridView
+                    var imageFiles = Directory.GetFiles(folderDialog.SelectedPath, "*.*")
+                                               .Where(f => f.EndsWith(".jpg") || f.EndsWith(".png") || f.EndsWith(".jpeg"))
+                                               .Select(f => new { FileName = Path.GetFileName(f), FilePath = f })
+                                               .ToList();
+
+                    dataGridViewImages.DataSource = imageFiles;
+                }
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            string sourceFolder = txtFolderPath.Text;
+            if (string.IsNullOrEmpty(sourceFolder) || !Directory.Exists(sourceFolder))
+            {
+                MessageBox.Show("Please select a valid folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string destinationFolder = Path.Combine(AppContext.BaseDirectory, "UploadedImages");
+            Directory.CreateDirectory(destinationFolder);
+
+            using (var dbContext = new AppDbContext())
+            {
+                var rows = dataGridViewImages.Rows.Cast<DataGridViewRow>().ToList();
+                int totalFiles = rows.Count;
+                int processedFiles = 0;
+
+                progressBarUpload.Value = 0;
+                progressBarUpload.Maximum = totalFiles;
+
+                foreach (var row in rows)
+                {
+                    var fileName = Path.GetFileNameWithoutExtension(row.Cells["FileName"].Value.ToString());
+                    var filePath = row.Cells["FilePath"].Value.ToString();
+
+                    // Match file name with database item
+                    var item = dbContext.Items.FirstOrDefault(i => i.ItemName == fileName);
+                    if (item != null)
+                    {
+                        string destPath = Path.Combine(destinationFolder, Path.GetFileName(filePath));
+                        File.Copy(filePath, destPath, true);
+
+                        // Update database
+                        item.FilePath = destPath;
+                        dbContext.Entry(item).State = EntityState.Modified;
+                    }
+
+                    // Update progress
+                    processedFiles++;
+                    progressBarUpload.Value = processedFiles;
+
+                    // Refresh the progress bar to reflect updates
+                    progressBarUpload.Refresh();
+                }
+
+                dbContext.SaveChanges();
+            }
+
+            MessageBox.Show("Files uploaded and database updated successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            progressBarUpload.Value = 0; // Reset progress bar
+        }
+    }
+}
